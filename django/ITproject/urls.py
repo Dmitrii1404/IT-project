fd9a0ef42e748c52d519a23082269b4419544a6a
Dmitrii <dima1404sidorov@yandex.ru>
2024-04-25 00:46:15 +0300

    added django

--- django/ITproject/urls.py
+++ django/ITproject/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
     path("", include("movies.urls"))
 ]
 
