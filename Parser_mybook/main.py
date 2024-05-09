2a70aa3ac71f9f23f300dbe099324a5fbbb02519
Dmitrii <dima1404sidorov@yandex.ru>
2024-05-09 16:12:37 +0300

    Применил форматеры и убрал все файлы бд из репозитория

--- Parser_mybook/main.py
+++ Parser_mybook/main.py
@@ -9,7 +9,6 @@ import requests
 import sqlite3
 
 
-
 async def fetch(session, url, headers=None):
     async with session.get(url, headers=headers) as response:
         # Задержка отправления, чтоб не забанил сайт (и мб это помогает прогрузить номально страницу)
@@ -18,27 +17,29 @@ async def fetch(session, url, headers=None):
 
 
 async def parse_film(session, url, conn, cur, semaphore, count_):
-    max = 1100
+    max = 1100  # Ограничения для счетчика
 
-    ISBN = ''  # Код книги  ----
-    Name = ''  # Название   ----
-    Page = ''  # Количество страниц  ----
-    Age = ''  # Возрастное ограничение  ----
-    URL = ''  # Ссылка   ----
-    Genres = []  # Жанры  ----
-    Topic = []  # Тема  ----
-    Rating = ''  # Оценка  ----
-    Number_of_ratings = ''  # Количество оценок  ----
-    Description = ''  # Описание ----
-    Author = ''  # Автор  ----
+    ISBN = ""  # Код книги
+    Name = ""  # Название
+    Page = ""  # Количество страниц
+    Age = ""  # Возрастное ограничение
+    URL = ""  # Ссылка
+    Genres = []  # Жанры
+    Topic = []  # Тема
+    Rating = ""  # Оценка
+    Number_of_ratings = ""  # Количество оценок
+    Description = ""  # Описание
+    Author = ""  # Автор
     Similars = []  # Похожие книги
 
     user = fake_useragent.UserAgent().random
     header = {"user-agent": user}
-    URL = 'https://mybook.ru/' + url
+    URL = "https://mybook.ru/" + url
     cur.execute("SELECT * FROM books WHERE URL = ?", (url,))
     result = cur.fetchone()
-    if count_ <= max and result == None:  # чтоб ноут долго не простаивал, парсинг разделяю на части, так что условие name <= max просто для моего удобства
+    if (
+        count_ <= max and result == None
+    ):  # чтоб ноут долго не простаивал, парсинг разделяю на части, так что условие name <= max просто для моего удобства
         try:
             async with semaphore:
                 html = await fetch(session, URL, headers=header)
@@ -46,79 +47,97 @@ async def parse_film(session, url, conn, cur, semaphore, count_):
             print(f"Iteration: {count_}")
 
             try:
-                some_info = soup.find('div', class_='ant-row sc-1c0xbiw-1 cyJjtq')
-                Name = some_info.find('div', class_='m4n24q-0 hJyrxa').text
-                Author = some_info.find('div', class_='dey4wx-1 jVKkXg').text
+                some_info = soup.find("div", class_="ant-row sc-1c0xbiw-1 cyJjtq")
+                Name = some_info.find("div", class_="m4n24q-0 hJyrxa").text
+                Author = some_info.find("div", class_="dey4wx-1 jVKkXg").text
             except:
                 pass
 
             try:
-                some_info = soup.find('div', class_='ant-row sc-1c0xbiw-1 cyJjtq')
-                some_info = some_info.find('div', class_='ant-col sc-1c0xbiw-9 eSjGMZ')
-                Page = some_info.find_all('p', class_='lnjchu-1 dPgoNf')[0].text
-                if 'печат' not in Page:
-                    Page = 'Неизвестно'
-                Age = some_info.find_all('p', class_='lnjchu-1 dPgoNf')[3].text
+                some_info = soup.find("div", class_="ant-row sc-1c0xbiw-1 cyJjtq")
+                some_info = some_info.find("div", class_="ant-col sc-1c0xbiw-9 eSjGMZ")
+                Page = some_info.find_all("p", class_="lnjchu-1 dPgoNf")[0].text
+                if "печат" not in Page:
+                    Page = "Неизвестно"
+                Age = some_info.find_all("p", class_="lnjchu-1 dPgoNf")[3].text
             except:
                 pass
 
             try:
-                some_info = soup.find('div', class_='ant-col sc-1c0xbiw-5 lotch')
-                Rating = some_info.find('div', class_='sc-1s4c57r-0 goYpPi').text
-                Number_of_ratings = some_info.find('div', class_='sc-1c0xbiw-6 cyZcfr').text
+                some_info = soup.find("div", class_="ant-col sc-1c0xbiw-5 lotch")
+                Rating = some_info.find("div", class_="sc-1s4c57r-0 goYpPi").text
+                Number_of_ratings = some_info.find(
+                    "div", class_="sc-1c0xbiw-6 cyZcfr"
+                ).text
             except:
                 pass
 
             try:
-                some_info = soup.find('div', class_='iszfik-14 iSnZQd')
-                some_info_2 = some_info.find_all('div', class_='iszfik-15 BerVK')[1]
-                ISBN = some_info_2.find_all('dd', class_='iszfik-18 iEusfO')[0].text
+                some_info = soup.find("div", class_="iszfik-14 iSnZQd")
+                some_info_2 = some_info.find_all("div", class_="iszfik-15 BerVK")[1]
+                ISBN = some_info_2.find_all("dd", class_="iszfik-18 iEusfO")[0].text
                 try:
                     a = int(ISBN)
                 except:
-                    ISBN = ''
+                    ISBN = ""
             except:
                 pass
 
             try:
-                some_info = soup.find_all('div', class_='sc-1sg8rha-0 gHinNz')
-                some_info_1 = some_info[0].find_all('div', class_='sc-1sbv3y7-0 bQSldI')
+                some_info = soup.find_all("div", class_="sc-1sg8rha-0 gHinNz")
+                some_info_1 = some_info[0].find_all("div", class_="sc-1sbv3y7-0 bQSldI")
                 for genre in some_info_1:
                     Genres.append(genre.text)
 
-                some_info_2 = some_info[1].find_all('div', class_='sc-1sbv3y7-0 bQSldI')
+                some_info_2 = some_info[1].find_all("div", class_="sc-1sbv3y7-0 bQSldI")
                 for tp in some_info_2:
                     Topic.append(tp.text)
             except:
                 pass
 
             try:
-                some_info = soup.find('div', class_='iszfik-2 gAFRve')
+                some_info = soup.find("div", class_="iszfik-2 gAFRve")
                 Description = some_info.text
             except:
                 pass
 
             try:
-                some_info = soup.find('div', class_='m4n24q-0 dsNpLo cy-similar-content-slider')
-                some_info = some_info.find('div', class_='sc-1hf4y1s-2 bTSqUP swiper-wrapper')
-                some_info = some_info.find_all('div', class_='swiper-slide sc-12qqvjh-0 gzajCe')
+                some_info = soup.find(
+                    "div", class_="m4n24q-0 dsNpLo cy-similar-content-slider"
+                )
+                some_info = some_info.find(
+                    "div", class_="sc-1hf4y1s-2 bTSqUP swiper-wrapper"
+                )
+                some_info = some_info.find_all(
+                    "div", class_="swiper-slide sc-12qqvjh-0 gzajCe"
+                )
                 for info in some_info:
-                    similar = info.find('div', class_='sc-7dmtki-0').find('a').get('href')
+                    similar = (
+                        info.find("div", class_="sc-7dmtki-0").find("a").get("href")
+                    )
                     Similars.append(similar)
             except:
                 pass
 
             try:
-                photo_ = soup.find('div', class_='hh1ehr-0 kkiIwl').find('picture').find('img')
-                imglink = photo_.get('srcset').split(', ')[0]
+                photo_ = (
+                    soup.find("div", class_="hh1ehr-0 kkiIwl")
+                    .find("picture")
+                    .find("img")
+                )
+                imglink = photo_.get("srcset").split(", ")[0]
                 image = requests.get(imglink).content
-                with open(r'C:/Users/Dmitrii/Desktop/data/imagine_books/' + Name + '.jpg', 'wb') as imgfile:
+                with open(
+                    r"C:/Users/Dmitrii/Desktop/data/imagine_books/" + Name + ".jpg",
+                    "wb",
+                ) as imgfile:
                     imgfile.write(image)
             except:
                 pass
 
-            print(ISBN, Name, Rating) # чтоб понимать, парсится или нет
-            cur.execute(f"""INSERT INTO books(
+            print(ISBN, Name, Rating)  # чтоб понимать, парсится или нет
+            cur.execute(
+                f"""INSERT INTO books(
                 ISBN,
                 Name,
                 Page,
@@ -131,20 +150,22 @@ async def parse_film(session, url, conn, cur, semaphore, count_):
                 Description,
                 Author,
                 Similars) 
-                              VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""", (
-                ISBN,
-                Name,
-                Page,
-                Age,
-                url,
-                ','.join(Genres),
-                ','.join(Topic),
-                Rating,
-                Number_of_ratings,
-                Description,
-                Author,
-                ','.join(Similars)
-            ))
+                              VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
+                (
+                    ISBN,
+                    Name,
+                    Page,
+                    Age,
+                    url,
+                    ",".join(Genres),
+                    ",".join(Topic),
+                    Rating,
+                    Number_of_ratings,
+                    Description,
+                    Author,
+                    ",".join(Similars),
+                ),
+            )
             conn.commit()
 
         except Exception as e:
@@ -152,18 +173,23 @@ async def parse_film(session, url, conn, cur, semaphore, count_):
 
     elif count_ <= max:
         count_ += 1
-        print(f'Книга с ссылкой {url} уже есть в дб')
+        print(f"Книга с ссылкой {url} уже есть в дб")
 
 
 async def main():
-    semaphore = asyncio.Semaphore(15)  # Ограничение на количество одновременно выполняемых задач
+    semaphore = asyncio.Semaphore(
+        15
+    )  # Ограничение на количество одновременно выполняемых задач
     async with aiohttp.ClientSession() as session:
-        with open("book_urls.json", "r", encoding="UTF-8") as file:
+        with open(
+            "C:/Users/Dmitrii/Desktop/data/DB/book_urls.json", "r", encoding="UTF-8"
+        ) as file:
             all_films = json.load(file)
 
-        conn = sqlite3.connect('books_info.db')
+        conn = sqlite3.connect("C:/Users/Dmitrii/Desktop/data/DB/books_info.db")
         cur = conn.cursor()
-        cur.execute('''
+        cur.execute(
+            """
                CREATE TABLE IF NOT EXISTS books (
                ISBN TEXT NOT NULL,
                Name TEXT NOT NULL,
@@ -178,7 +204,8 @@ async def main():
                Author TEXT NOT NULL,
                Similars TEXT NOT NULL
                )
-               ''')
+               """
+        )
         conn.commit()
         tasks = []
         count = 0
